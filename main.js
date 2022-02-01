a3d63116b18e85097c5cb68197bc9ac396b5283d
Vu Dinh <vuniverse143@gmail.com>
2022-01-31 18:46:16 -0800

    working mobile view 1.0

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+function invertHeaderColor() {
+  var headerElement = document.querySelector("header");
+  var bottomElement = document.querySelector(".copyright");
+  var windowHeight = window.innerHeight;
+  var elementTop = bottomElement.getBoundingClientRect().top;
+  var elementVisible = (windowHeight / 10) * 1;
+  if (elementTop < windowHeight - elementVisible + 80) {
+    // kinda hacky
+    headerElement.classList.add("invert");
+  } else {
+    headerElement.classList.remove("invert");
+  }
+}
+
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+    var elementVisible = (windowHeight / 10) * 1;
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     } else {
@@ -18,7 +32,7 @@ function hide() {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
     var elementVisible = 150;
-    var specialOffset = 500;
+    var specialOffset = (windowHeight / 8) * 3;
     if (elementTop < windowHeight - elementVisible - specialOffset) {
       reveals[i].classList.add("active");
     } else {
@@ -29,3 +43,4 @@ function hide() {
 
 window.addEventListener("scroll", reveal);
 window.addEventListener("scroll", hide);
+window.addEventListener("scroll", invertHeaderColor);
